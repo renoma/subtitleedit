be584c0f3f0bdda9ac30b9a8d16712c9360c0e49
RanomA Kim <ranoma@ranoma.com>
2021-05-26 10:20:05 +0900

    초기 버전 생성(3.6.1)

---
+++ src/libse/Forms/FixCommonErrors/FixInvalidItalicTags.cs
@@ -0,0 +1,51 @@
+﻿using Nikse.SubtitleEdit.Core.Common;
+using Nikse.SubtitleEdit.Core.Interfaces;
+
+namespace Nikse.SubtitleEdit.Core.Forms.FixCommonErrors
+{
+    public class FixInvalidItalicTags : IFixCommonError
+    {
+        public static class Language
+        {
+            public static string FixInvalidItalicTag { get; set; } = "Fix invalid italic tag";
+            public static string FixInvalidItalicTags { get; set; } = "Fix invalid italic tags";
+        }
+
+        public void Fix(Subtitle subtitle, IFixCallbacks callbacks)
+        {
+            const string beginTagUpper = "<I>";
+            const string endTagUpper = "</I>";
+            const string beginTag = "<i>";
+            const string endTag = "</i>";
+            string fixAction = Language.FixInvalidItalicTag;
+            int noOfInvalidHtmlTags = 0;
+            for (int i = 0; i < subtitle.Paragraphs.Count; i++)
+            {
+                if (callbacks.AllowFix(subtitle.Paragraphs[i], fixAction))
+                {
+                    var text = subtitle.Paragraphs[i].Text;
+                    if (text.Contains('<'))
+                    {
+                        if (!text.Contains("i>") && !text.Contains("<i"))
+                        {
+                            text = text.Replace(beginTagUpper, beginTag).Replace(endTagUpper, endTag);
+                        }
+
+                        string oldText = text;
+
+                        text = HtmlUtil.FixInvalidItalicTags(text);
+                        if (text != oldText)
+                        {
+                            subtitle.Paragraphs[i].Text = text;
+                            noOfInvalidHtmlTags++;
+                            callbacks.AddFixToListView(subtitle.Paragraphs[i], fixAction, oldText, text);
+                        }
+                    }
+                }
+            }
+
+            callbacks.UpdateFixStatus(noOfInvalidHtmlTags, Language.FixInvalidItalicTags);
+        }
+
+    }
+}
---
+++ src/ui/Forms/Styles/StylesForm.cs
@@ -0,0 +1,52 @@
+﻿using Nikse.SubtitleEdit.Core;
+using System;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.Forms.Styles
+{
+    public /* abstract */ class StylesForm : Form
+    {
+        private readonly Timer _previewTimer = new Timer();
+
+        private StylesForm()
+        {
+            // Only used by the Visual Studio designer.
+        }
+
+        protected StylesForm(Subtitle subtitle)
+        {
+            Subtitle = subtitle;
+
+            _previewTimer.Interval = 200;
+            _previewTimer.Tick += PreviewTimerTick;
+        }
+
+        public virtual string Header => throw new NotImplementedException("This property getter has to be overridden.");
+
+        protected Subtitle Subtitle { get; }
+
+        protected void GeneratePreview()
+        {
+            if (_previewTimer.Enabled)
+            {
+                _previewTimer.Stop();
+                _previewTimer.Start();
+            }
+            else
+            {
+                _previewTimer.Start();
+            }
+        }
+
+        protected virtual void GeneratePreviewReal()
+        {
+            throw new NotImplementedException("This method has to be overridden.");
+        }
+
+        private void PreviewTimerTick(object sender, EventArgs e)
+        {
+            _previewTimer.Stop();
+            GeneratePreviewReal();
+        }
+    }
+}
